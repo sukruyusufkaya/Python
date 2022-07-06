c128b4c77101eaed073c88a6d48b2b70d3a9cb46
sukruyusufkaya <89349076+sukruyusufkaya@users.noreply.github.com>
2022-07-06 16:35:26 +0300

    Update AlarmClock.py

--- AlarmClock.py
+++ AlarmClock.py
@@ -1,27 +1,3 @@
-"""from openpyxl import Workbook,load_workbook
-
-P_yuk = [0.580416667, 0.539066667, 0.390116667, 0.232033333, 0.204533333, 0.194716667, 0.194633333, 0.209233333, 0.247266668, 0.407916668, 0.537349998, 0.576983332, 0.580216667, 0.520566667, 0.485200003, 0.4197, 0.424300002, 0.448333332, 0.546983333, 0.840733333, 1.0, 0.856422014, 0.921716667, 0.720283335]
-pv_uretilen = [0, 0, 0, 0, 0.846573268, 6.670823882, 0.2234096457, 0.4840323145, 0.9510129002, 1.617686087, 2.369894473, 2.939150696, 3.053854497, 2.946843366, 2.517269744, 1.822991627,1.23210826, 7.311869927, 3.355642336, 0.9910144956, 0.1621109317, 0.008980831, 0, 0]
-P_sebeke_satis = list()
-P_sebeke_alis = list()
-for i in range(0, 24):
-    if pv_uretilen[i] < P_yuk[i]:
-        P_sebeke_alis.append((P_yuk[i] - pv_uretilen[i]))
-        P_sebeke_satis.append(0)
-
-    elif pv_uretilen[i] > P_yuk[i]:
-        P_sebeke_satis.append((pv_uretilen[i] - P_yuk[i]))
-        P_sebeke_alis.append(0)
-
-Guc_sebekedenalinan = 0
-for i in range(0, 24):
-    print(i,".00 Saatinde şebekeden alınan yük", P_sebeke_alis)
-    #Guc_sebekedenalinan = Guc_sebekedenalinan + int(P_sebeke_alis[i])
-
-Guc_sebekeyeverilen = 0
-for i in range(0, 24):
-    print(i,".00 Saatinde şebekeye satilan yük", P_sebeke_satis)
-    #Guc_sebekeyeverilen = Guc_sebekeyeverilen + int(P_sebeke_satis[i])"""
 from tkinter import *
 import datetime
 import time
@@ -62,7 +38,7 @@ secTime = Entry(clock,textvariable = sec,bg = "pink",width = 15).place(x=200,y=3
 submit = Button(clock,text = "Set Alarm",fg="red",width = 10,command = actual_time).place(x =110,y=70)
 clock.mainloop()
 #Execution of the window.clock = Tk()
-clock.title("DataFlair Alarm Clock")
+clock.title("Alarm Clock by Sukru Yusuf KAYA")
 clock.geometry("400x200")
 time_format=Label(clock, text= "Enter time in 24 hour format!", fg="red",bg="black",font="Arial").place(x=60,y=120)
 addTime = Label(clock,text = "Hour  Min   Sec",font=60).place(x = 110)
